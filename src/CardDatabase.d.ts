691b550a6401d1cb8f3ea00ec531594d5c5728d4
John <john@doxel.ai>
2022-03-12 17:53:16 +0900

    fixed type

--- src/CardDatabase.d.ts
+++ src/CardDatabase.d.ts
@@ -1,2 +1,2 @@
 import { QueryExecResult } from "sql.js";
-export function queryDictionary(query: string): QueryExecResult[];
+export function queryDictionary(query: string): Promise<QueryExecResult[]>;
